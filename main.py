87a63ecad5e631ad14d2f50ee2c22fea8830179e
angelsoempty <denatamanov1@gmail.com>
2023-05-02 20:11:39 +0300

    #2

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-def add_numb(func):
+import time
+
+def time_function(func):
     def wrapper(*args, **kwargs):
-        return func(*args, **kwargs) + 10
+        stat_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print('Час виконання функції ', end_time - stat_time)
+        return result
     return wrapper
 
-@add_numb
-def number(n):
-    return n
-
-print(number(5))
+@time_function
+def example_function(x):
+    time.sleep(1)#pass
+    return x
+print(example_function(0.1))
