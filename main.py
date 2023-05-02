370fa81879fc4b93f0d6f4d7d90cd7241593d34a
angelsoempty <denatamanov1@gmail.com>
2023-05-02 19:02:17 +0300

    таймер 3 секунди

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ def my_decorator_func(func): #3
 def say_hello(): #1
     print('Hello!')
 say_hello()
+
+import time
+
+def delay_decorator(func):
+    def wrapper(*args, **kwargs):
+        time.sleep(3)
+        return func(*args, **kwargs)
+    return wrapper
+
+@delay_decorator
+def sleepy():
+    print('я сплю')
+sleepy()
+
