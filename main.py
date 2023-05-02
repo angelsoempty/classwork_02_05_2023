ddc73a3adf16d7107cdf4188722982a246544557
angelsoempty <denatamanov1@gmail.com>
2023-05-02 20:03:29 +0300

    #1

--- main.py
+++ main.py
@@ -1,42 +1,10 @@
-#теорія Декоратори
-
-def my_decorator_func(func): #3
-    def wrapper(): #4
-        print('shoto tam')
-        func()
-        print('i tam shos')
-    return wrapper
-
-
-@my_decorator_func #2
-def say_hello(): #1
-    print('Hello!')
-say_hello()
-
-import time
-
-def delay_decorator(func):
+def add_numb(func):
     def wrapper(*args, **kwargs):
-        time.sleep(.1)
-        return func(*args, **kwargs)
+        return func(*args, **kwargs) + 10
     return wrapper
 
-@delay_decorator
-def sleepy():
-    print('я сплю')
-sleepy()
+@add_numb
+def number(n):
+    return n
 
-
-def cache_decorator(func):
-    cache = {}
-    def wrapper(n):
-        if n not in cache:
-            cache[n] = func(n)
-        return cache[n], cache
-    return wrapper
-@cache_decorator
-def fibonacci(n):
-    if n in (0, 1):
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
-print(fibonacci(10))
+print(number(5))
