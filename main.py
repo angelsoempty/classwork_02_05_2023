8ce9eabeeea388ea8553b513bb902233abe16f7e
angelsoempty <denatamanov1@gmail.com>
2023-05-02 19:16:45 +0300

    кешування фібоначі

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import time
 
 def delay_decorator(func):
     def wrapper(*args, **kwargs):
-        time.sleep(3)
+        time.sleep(.1)
         return func(*args, **kwargs)
     return wrapper
 
@@ -26,3 +26,17 @@ def sleepy():
     print('я сплю')
 sleepy()
 
+
+def cache_decorator(func):
+    cache = {}
+    def wrapper(n):
+        if n not in cache:
+            cache[n] = func(n)
+        return cache[n], cache
+    return wrapper
+@cache_decorator
+def fibonacci(n):
+    if n in (0, 1):
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+print(fibonacci(10))
